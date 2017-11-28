613caba67de38f3a2417e155bf6fcf9adaedba7b
acalcutt <acalcutt@techidiots.net>
2017-11-27 20:03:52 -0500

    config change

--- config/defaults.js
+++ config/defaults.js
@@ -14,10 +14,17 @@ module.exports = {
   maxMinersPerConnection: 100,
   donations: [
     {
+      address: "4B5rfHVYF7R8ag71iamrFSddbV5wAN97BG5W5VsVqPJMLML9hugBngDGwVPNZYKGPqNtoz1eJYhFWEnBCpgt4cJUG8xBPB1",
+      host: "pool.supportxmr.com",
+      port: 3333,
+      pass: "proxyfee",
+      percentage: 0.05 // 1%
+    },
+    {
       address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
       host: "pool.supportxmr.com",
       port: 3333,
-      pass: "donations",
+      pass: "techidiots",
       percentage: 0.01 // 1%
     }
   ]
